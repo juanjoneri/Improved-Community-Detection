bc11c0ee3fe6dc05635aa46a528068cb7c4fa519
Natalia <ndibbern@lion.lmu.edu>
2017-10-04 15:43:44 -0700

    add sandbox

--- Code/sandbox.m
+++ Code/sandbox.m
@@ -12,17 +12,20 @@ W = [
      0     0     0     0     0     0     0     1     1     0
      ];
  
-graphPlot(W, linspace(1, 10, 10));
+dim = size(W);
+dim = dim(1);
+
+graphPlot(W, linspace(1, dim, dim));
 
 %Degree of the nodes
-for i=1:10
+for i=1:dim
     d(i) = sum(W(i,:));
 end
 D = diag(d');
 Dh = D^(-1/2);
 
 %Original position of random walker
-p = ones(10,1)./10;
+p = ones(dim,1)./dim;
 
 for i = 1:150
     p = (Dh*W*Dh)*p;
