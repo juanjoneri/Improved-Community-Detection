99f16df57abe78a434a7c4e40b5a212bb15438b2
Juan Neri <juanjo.neri@gmail.com>
2017-10-04 18:02:38 -0700

    plot of graph after DWD

--- Code/graphPlot.m
+++ Code/graphPlot.m
@@ -3,6 +3,9 @@
 % Makes a graph out of an adjacency matrix and plots it.
 
 function p = graphPlot( A, w )
+    f = figure
     p = plot(graph(A));
     p.NodeCData = w;
+    colorbar
+    print(f, 'MySavedPlot','-dpng')
 end
