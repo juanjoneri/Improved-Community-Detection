d9a29a618c9d63413a5efba5c5db610cce9341f6
Juan neri <juanjo.neri@gmail.com>
2018-01-09 14:35:27 -0300

    make partition with a ac in illustrator, generate graphics missing

--- Code/Python/sandbox.py
+++ Code/Python/sandbox.py
@@ -1,20 +1,8 @@
-import numpy as np
-import tensorflow as tf
-
-
-
-
-
-def create_group(R, r):
-    group = np.zeros((1,R))
-    group[0,r] = 1
-    print(group)
-    return tf.Variable(group, dtype=tf.float64)
-
+from my_packages.clusters.save_cluster import *
+from my_packages.clusters.plot_cluster import plot_G
 
 if __name__ == '__main__':
-    with tf.Session() as sess:
-        sess.run(tf.global_variables_initializer())
-        
-        g = sess.run(create_group(4,2))
-        print(g)
+    H = import_cluster('./my_packages/clusters/examples/120-4/120n-4c-cluster.csv')
+    h_coordinates, h_labels = import_metadata('./my_packages/clusters/examples/120-4/120n-4c-meta.csv')
+    plot_G(H, h_coordinates)
+    plot_G(H, h_coordinates, h_labels)
