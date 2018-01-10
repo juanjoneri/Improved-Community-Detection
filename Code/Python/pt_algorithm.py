facb19ce0218059ae7051dd9c07ce94974ddc702
Juan neri <juanjo.neri@gmail.com>
2018-01-09 22:45:33 -0800

    reseeding working

--- Code/Python/pt_algorithm.py
+++ Code/Python/pt_algorithm.py
@@ -17,16 +17,37 @@ class Algorithm:
         self.n = self.W.size()[0]       # Number of vertexes
         self.a = torch.FloatTensor([a])   # Alpha: diffusion parameter
 
-    def diffuse(self):
+    def diffuse(self, iterations):
         # Apply one diffuse step to the current heat distribution H
         I = torch.eye(self.n)
         D_ = torch.diag(torch.pow(self.D, -0.5))
         Op = torch.mm(D_, torch.mm(self.W, D_)).type(torch.FloatTensor)
-        self.H = torch.mul(self.a, torch.mm(Op, self.H)) + torch.mul((1 - self.a), self.F)
+        for _ in range(iterations):
+            self.H = torch.mul(self.a, torch.mm(Op, self.H)) + torch.mul((1 - self.a), self.F)
 
     def reseed(self, seeds):
-        #Take the k (`seeds`) best seeds from each heat bump specified by H
-        pass
+        # Take the k (`seeds`) best seeds from each heat bump specified by H
+        allocated = set() # Track allocated nodes
+        nodes_per_class = dict(zip(range(self.R), [0]*self.R)) # Track full classes
+
+        ranks = torch.topk(self.H, self.n, dim=0)[1]
+        new_F = torch.zeros(n_nodes, n_clusters)
+
+        i = 0
+        for rank in ranks:
+            j = 0
+            for node in rank:
+                if node not in allocated and nodes_per_class[j] < seeds:
+                    allocated.add(node)
+                    nodes_per_class[j] += 1
+                    new_F[node][j] = 1
+                j += 1
+            i += 1
+
+        print(ranks)
+        print(allocated)
+        print(new_F)
+
 
     def labels(self):
         # Return a vector with labels for each class (only makes sense when F represents a partition)
@@ -43,9 +64,10 @@ if __name__ == '__main__':
     graph_W = torch.from_numpy(small_W)
     initial_F = torch.zeros(n_nodes, n_clusters)
     initial_F[1][0] = 1
-    initial_F[12][1] = 1
+    initial_F[9][1] = 1
 
     algorithm = Algorithm(graph_W, initial_F, R=n_clusters, a=0.9)
-    algorithm.diffuse()
+    algorithm.diffuse(10)
 
+    print(algorithm.reseed(6))
     plot_G(G, coordinates)
